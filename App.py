85ffb76ef6199875694d046a7f714e0271af07cd
Adam Sommer <asommer70@gmail.com>
2018-04-12 16:09:52 -0400

    Learning more about layouts in Kivy.

--- App.py
+++ App.py
@@ -5,13 +5,14 @@ from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 
-class Layout(FloatLayout):
+class Grid(GridLayout):
     def press_beans(self, *args):
         print('self.ids:', self.ids)
 
@@ -27,21 +28,38 @@ class Settings(GridLayout):
         self.add_widget(self.url)
 
 
+class Anchor(AnchorLayout):
+    pass
+
+class Float(FloatLayout):
+    pass
+
+
 class EvergreenNewReleases(App):
     def build(self):
         # Builder.load_file('./app.kv')
         # return Label()
         # return Settings()
-        layout = Layout()
 
-        beans_button = Button(
-            text='Beans button...',
-            size_hint=(0.2, 0.2),
-            on_press=layout.press_beans,
-            pos_hint={'x': 0, 'y': 0}
-        )
-        layout.add_widget( beans_button)
-        return layout
+        # grid = Grid(cols=3, padding=14, spacing=14)
+        # grid.row_default_height = 100
+        # grid.col_default_width = 75
+        # grid.col_force_default = True
+        # grid.row_force_default = True
+        #
+        # beans_button = Button(
+        #     text='Beans button...',
+        #     size_hint=(0.2, 0.2),
+        #     on_press=grid.press_beans,
+        #     pos_hint={'x': 0, 'y': 0}
+        # )
+        # grid.add_widget( beans_button)
+
+        # anchor = Anchor(anchor_x='center', anchor_y='top')
+        # anchor.add_widget( Button(text='center top', background_color=(1,0,0,1), size_hint=(None, None), size=(50,50) ))
+        floater = Float()
+        floater.add_widget(anchor)
+        return floater
 
 
 if __name__ == '__main__':
